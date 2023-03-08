3261fdfcc367fc0e12492d5aaedabd0642ae9b0c
Leanne Tay <duckingville@yahoo.com>
2023-03-08 17:28:09 +0800

    added flats index to display all flats

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,13 @@
 class FlatsController < ApplicationController
+  before_action :set_flats, only: %i[index]
+
+  def index
+    @flats
+  end
+
+  private
+
+  def set_flats
+    @flats = Flat.all
+  end
 end
